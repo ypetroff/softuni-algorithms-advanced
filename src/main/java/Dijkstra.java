6f911d07660d215c3dba1830d5dee871f21d6bb7
Yordan Petrov <yordan.petroff@gmail.com>
2023-07-11 11:52:58 +0300

    Dijkstra algorithm implemented

--- src/main/java/Dijkstra.java
+++ src/main/java/Dijkstra.java
@@ -1,8 +1,57 @@
-import java.util.List;
+import java.util.*;
 
 public class Dijkstra {
 
-    public static List<Integer> dijkstraAlgorithm(int[][] graph, int sourceNode, int destinationNode) {
-        return null;
+  public static List<Integer> dijkstraAlgorithm(
+      int[][] graph, int sourceNode, int destinationNode) {
+    int[] distance = new int[graph.length];
+    boolean[] visited = new boolean[graph.length];
+    int[] prev = new int[graph.length];
+
+    Arrays.fill(prev, -1);
+
+    Arrays.fill(distance, Integer.MAX_VALUE);
+    distance[sourceNode] = 0;
+
+    PriorityQueue<Integer> queue =
+        new PriorityQueue<>(Comparator.comparingInt(node -> distance[node]));
+    queue.offer(sourceNode);
+
+    while (!queue.isEmpty()) {
+      int parentNode = queue.poll();
+      visited[parentNode] = true;
+
+      if (distance[parentNode] == Integer.MAX_VALUE) {
+        break;
+      }
+
+      int[] children = graph[parentNode];
+
+      for (int childNode = 0; childNode < children.length; childNode++) {
+        if (!visited[childNode] && children[childNode] != 0) {
+          queue.offer(childNode);
+          int newDistance = distance[parentNode] + graph[parentNode][childNode];
+
+          if (newDistance < distance[childNode]) {
+            distance[childNode] = newDistance;
+            prev[childNode] = parentNode;
+          }
+        }
+      }
     }
+
+    List<Integer> path = new ArrayList<>();
+
+    path.add(destinationNode);
+
+    int n = prev[destinationNode];
+
+    while (n != -1) {
+      path.add(n);
+      n = prev[n];
+    }
+
+    Collections.reverse(path);
+    return path.size() > 1 ? path : null;
+  }
 }
--- src/test/java/DijkstraTests.java
+++ src/test/java/DijkstraTests.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class DijkstraTests {
 
-    private static int[][] GRAPH =
+    private static final int[][] GRAPH =
     {
         // 0   1   2   3   4   5   6   7   8   9  10  11
         { 0,  0,  0,  0,  0,  0, 10,  0, 12,  0,  0,  0 }, // 0
