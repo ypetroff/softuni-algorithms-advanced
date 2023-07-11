d7756c3cf56091f3eacb6b22c8dd82923486f7c9
Yordan Petrov <yordan.petroff@gmail.com>
2023-07-11 15:32:26 +0300

    Kruskal algorithm implemented

--- src/main/java/KruskalAlgorithm.java
+++ src/main/java/KruskalAlgorithm.java
@@ -1,12 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class KruskalAlgorithm {
 
     public static List<Edge> kruskal(int numberOfVertices, List<Edge> edges) {
-        return null;
+        /*
+        We can use PriorityQueue, but in cases with edges with equal weight
+        the results will differ. Nevertheless, both will provide the minimum spanning tree.
+        * */
+        Collections.sort(edges);
+
+        List<Edge> forest = new ArrayList<>();
+        int[] parents = new int[numberOfVertices];
+
+    for (int i = 0; i < parents.length; i++) {
+        parents[i] = i;
+    }
+
+    while (!edges.isEmpty()) {
+        Edge edge = edges.remove(0);
+
+        int source = edge.getStartNode();
+        int destination = edge.getEndNode();
+
+        int firstRoot = findRoot(source, parents);
+        int secondRoot = findRoot(destination, parents);
+
+        if (firstRoot != secondRoot) {
+            forest.add(edge);
+            parents[firstRoot] = secondRoot;
+        }
+    }
+
+        return forest;
     }
 
     public static int findRoot(int node, int[] parents) {
-        return 0;
+        while (parents[node] != node) {
+            node = parents[node];
+        }
+        return node;
     }
 }
