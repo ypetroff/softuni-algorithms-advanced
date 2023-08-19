76a898ca398d06e141cbe11fadaaff7511463e91
Yordan Petrov <yordan.petroff@gmail.com>
2023-08-19 22:57:23 +0300

    LCS dp solution

---
+++ src/main/java/_06_dynamic_programming_advanced/_02_LongestCommonSubsequence.java
@@ -0,0 +1,26 @@
+package _06_dynamic_programming_advanced;
+
+import java.util.Scanner;
+
+public class _02_LongestCommonSubsequence {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String first = scanner.nextLine();
+    String second = scanner.nextLine();
+
+    int[][] dp = new int[first.length() + 1][second.length() + 1];
+
+    for (int rowIndex = 1; rowIndex <= first.length(); rowIndex++) {
+      for (int colIndex = 1; colIndex <= second.length(); colIndex++) {
+        if (first.charAt(rowIndex - 1) == second.charAt(colIndex - 1)) {
+          dp[rowIndex][colIndex] = dp[rowIndex - 1][colIndex - 1] + 1;
+        } else {
+          dp[rowIndex][colIndex] = Math.max(dp[rowIndex - 1][colIndex], dp[rowIndex][colIndex - 1]);
+        }
+      }
+    }
+    System.out.println(dp[first.length()][second.length()]);
+  }
+}
